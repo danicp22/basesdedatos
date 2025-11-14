a5dcc0629ea11b9662156a2929586302ec658aa7
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-14 13:48:27 +0100

    Add files via upload

---
+++ 003-Realizacion de consultas/001-Proyeccion, seleccion y ordenacion de registros/001-creo una base de datos.sql
@@ -0,0 +1,46 @@
+-- sudo mysql -u root -p
+
+CREATE DATABASE clientes;
+
+USE clientes;
+
+CREATE TABLE clientes(
+  nombre VARCHAR(255),
+  apellidos VARCHAR(255),
+  edad INT
+);
+
+INSERT INTO clientes VALUES ("Juan","Lopez",45);
+INSERT INTO clientes VALUES ("Javier","Martinez",46);
+-- podéis usar IA para crear más inserts
+
+INSERT INTO clientes VALUES ("Maria","Gomez",32);
+INSERT INTO clientes VALUES ("Ana","Torres",28);
+INSERT INTO clientes VALUES ("Luis","Fernandez",51);
+INSERT INTO clientes VALUES ("Carlos","Ramirez",39);
+INSERT INTO clientes VALUES ("Sofia","Diaz",24);
+INSERT INTO clientes VALUES ("Lucia","Vargas",41);
+INSERT INTO clientes VALUES ("Pedro","Hernandez",36);
+INSERT INTO clientes VALUES ("Marta","Santos",29);
+INSERT INTO clientes VALUES ("Elena","Cruz",47);
+INSERT INTO clientes VALUES ("Raul","Navarro",33);
+INSERT INTO clientes VALUES ("Alejandro","Castro",38);
+INSERT INTO clientes VALUES ("Daniel","Ruiz",42);
+INSERT INTO clientes VALUES ("Patricia","Reyes",27);
+INSERT INTO clientes VALUES ("Esteban","Mora",53);
+INSERT INTO clientes VALUES ("Claudia","Rojas",31);
+INSERT INTO clientes VALUES ("Fernando","Silva",55);
+INSERT INTO clientes VALUES ("Andrea","Campos",26);
+INSERT INTO clientes VALUES ("Ruben","Lozano",44);
+INSERT INTO clientes VALUES ("Beatriz","Roman",30);
+INSERT INTO clientes VALUES ("Adrian","Delgado",35);
+INSERT INTO clientes VALUES ("Natalia","Peña",23);
+INSERT INTO clientes VALUES ("Jorge","Iglesias",48);
+INSERT INTO clientes VALUES ("Rosa","Serrano",37);
+INSERT INTO clientes VALUES ("Pablo","Ortega",52);
+INSERT INTO clientes VALUES ("Irene","Suarez",25);
+INSERT INTO clientes VALUES ("Hugo","Marin",34);
+INSERT INTO clientes VALUES ("Carmen","Calvo",49);
+INSERT INTO clientes VALUES ("Victor","Robles",40);
+INSERT INTO clientes VALUES ("Alba","Benitez",22);
+INSERT INTO clientes VALUES ("Sergio","Velasco",45);
