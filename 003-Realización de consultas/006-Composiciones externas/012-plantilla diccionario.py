e28ef7a993d6d1f19fed30a2e3cab86d9ec1e511
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-26 14:03:22 +0100

    1

---
+++ 003-Realización de consultas/006-Composiciones externas/012-plantilla diccionario.py
@@ -0,0 +1,15 @@
+import mysql.connector 
+
+conexion = mysql.connector.connect(
+  host="localhost",
+  user="composiciones",
+  password="Composiciones12345&",
+  database="composiciones"
+)                                      
+  
+cursor = conexion.cursor(dictionary=True) 
+cursor.execute("SELECT * FROM matriculas_join;")  
+
+filas = cursor.fetchall()
+
+print(filas)
---
+++ 003-Realización de consultas/006-Composiciones externas/013-flask para lanzar web.py
@@ -0,0 +1,21 @@
+import mysql.connector 
+from flask import Flask
+
+conexion = mysql.connector.connect(
+  host="localhost",
+  user="composiciones",
+  password="Composiciones12345&",
+  database="composiciones"
+)                                      
+
+app = Flask(__name__)
+@app.route("/")
+def inicio():
+  cursor = conexion.cursor(dictionary=True) 
+  cursor.execute("SELECT * FROM matriculas_join;")  
+  filas = cursor.fetchall()
+  return str(filas)
+
+if __name__ == "__main__":
+  # Pon en marcha la aplicación
+  app.run(debug=True)
